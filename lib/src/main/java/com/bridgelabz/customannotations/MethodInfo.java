596eb1624af58bfdd2aa77f11768d4b5486b44c8
shruthishruu05 <shruthi.m@perfios.com>
2021-09-20 17:25:51 +0530

    ADDED : Project Structure

---
+++ lib/src/main/java/com/bridgelabz/customannotations/MethodInfo.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.customannotations;
+
+import java.lang.annotation.*;
+
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodInfo {
+	String author() default "Narayan";
+	String date();
+	int revision() default 1;
+	String comments();
+}
